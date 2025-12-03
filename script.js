3d544b1706bf12c130e65218dfa07a69b80136cd
Rami <ramirazaq@gmail.com>
2025-12-02 21:32:56 -0600

    Revert mobile redesign changes

--- script.js
+++ script.js
@@ -99,7 +99,6 @@ class AnimalStatsApp {
             },
             
             detailsPanel: document.getElementById('details-panel'),
-            detailsCloseBtn: document.getElementById('details-close-btn'),
             expandDetailsBtn: document.getElementById('expand-details-btn'),
             detailText: {
                 type: document.getElementById('detail-type'),
@@ -334,11 +333,6 @@ class AnimalStatsApp {
         // UI Toggles
         if (this.dom.expandDetailsBtn) {
             this.dom.expandDetailsBtn.addEventListener('click', this.toggleDetails);
-        }
-        if (this.dom.detailsCloseBtn) {
-            this.dom.detailsCloseBtn.addEventListener('click', () => {
-                if (this.state.isDetailsExpanded) this.toggleDetails();
-            });
         }
         if (this.dom.toggleGridBtn) {
             this.dom.toggleGridBtn.addEventListener('click', this.toggleGrid);
